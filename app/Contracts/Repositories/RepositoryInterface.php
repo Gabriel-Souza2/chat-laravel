4650209bc5e13a05c4f30b1ce242e6559d93cd55
Gabriel Souza <gabriel.souza2@hotmail.com.br>
2019-11-19 10:36:49 -0300

    Criando classe Repository e Repository Interface

---
+++ app/Contracts/Repositories/RepositoryInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Contracts\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Collection;
+
+interface RepositoryInterface {
+
+    function create(array $data): Model;
+
+    function find(int $id): Model;
+
+    function all(): Collection;
+
+    function update(int $id, array $data): Model;
+
+    function delete(int $id): Bool;
+}
---
+++ app/Repositories/Repository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Repositories; 
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Collection;
+use App\Contracts\Repositories\RepositoryInterface;
+
+abstract class Repository implements RepositoryInterface 
+{
+    protected abstract function getModel();
+
+    public function create(array $data): Model
+    {
+        return $this->getModel()->create($data);
+    }
+
+    public function find(int $id): Model
+    {
+        return $this->getModel()->find($id);
+    }
+
+    public function all(): Collection
+    {
+        return $this->getModel()->all();
+    }
+
+    public function update(int $id, array $data): Model
+    {
+        return $this->getModel()->find($id)->update($data);
+    }
+
+    public function delete(int $id): Bool
+    {
+        return $this->getModel()->find($id)->delete();
+    }
+}
